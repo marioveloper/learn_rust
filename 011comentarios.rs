a1e65a9a12b2f5bf3a5263d29b687149cb510d2e
unknown <marioveloper@gmail.com>
2025-04-20 14:41:06 -0400

    Comentarios

---
+++ 011comentarios.rs
@@ -0,0 +1,9 @@
+//comentario de una linea
+/*
+* Comentario
+* bonito
+*/
+fn main(){
+    let x = 5 + /* 90 + */ 5;
+    println!("Es `x` 10 o 100? x = {}", x);
+}
--- 01hello.rs
+++ 01hello.rs
@@ -1,3 +1,12 @@
+//comentario de una linea
+/*
+    Comentario en varias lineas
+*/
+
+/*
+* Comentario
+* bonito
+*/
 fn main(){
     println!("Hello world")
 }
